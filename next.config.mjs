286f50f25c99029f19bd930134fba7303f25c360
yesutkarsh <contactutkarshverma@gmail.com>
2024-09-12 15:00:24 +0530

    http issue sove

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://34.67.228.203:4000/:path*' // Proxy to your backend
+        }
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
